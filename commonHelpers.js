8776daa99dc6ce0d5c81e40f1880401314a98dbc
NazarKoshitsky <77495036+NazarKoshitsky@users.noreply.github.com>
2024-03-10 13:30:32 +0000

    Deploying to gh-pages from @ NazarKoshitsky/goit-js-hw-12@a0edb33cde1b389c1718848b9a6a5e12334af201 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,28 +1,27 @@
-import{a as w,i as p,S as $}from"./assets/vendor-5401a4b0.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=n(e);fetch(e.href,s)}})();function u(r){return r.hits.map(({largeImageURL:t,webformatURL:n,tags:o,likes:e,views:s,comments:i,downloads:b})=>`<div class="gallery-item>
-      <a class="gallery-link" href="${t}">
-          <img 
-          class="gallery-image" 
-          src="${n}" 
-          alt="${o}" 
-          width="360px" height="260px"/>
-      </a>
-      <div class="gallery-info">
-          <p class="gallery-info-par">
-              <span class="gallery-info-span">Likes: <span class="tag-span">${e}</span>
-              </span>    
-          </p>
-          <p class="gallery-info-par">
-              <span class="gallery-info-span">Views: <span class="tag-span">${s}</span>
-              </span>    
-          </p>
-          <p class="gallery-info-par">
-              <span class="gallery-info-span">Comments: <span class="tag-span">${i}</span>
-              </span>    
-          </p>
-          <p class="gallery-info-par">
-              <span class="gallery-info-span">Downloads: <span class="tag-span">${b}</span>
-              </span>    
-          </p>
-      </div>
-  </div>`).join("")}const v="42773289-557706e6fefb377aba1f00ed4",L="https://pixabay.com/api/",d=document.querySelector(".loader"),S=document.querySelector(".load-btn"),E=document.querySelector(".form");async function f(r,t,n){const o=`${L}?key=${v}&q=${r}&image_type=photo&orientation=horizontal&savesearch=true&page=${n}&per_page=${t}`;d.style.display="block";try{const e=await w.get(o);return e.data.hits.length===0&&(p.error({title:"Error",timeout:2e3,position:"bottomRight",message:"Sorry, there are no images matching your search query. Please try again!"}),S.style.display="none",d.style.display="none",E.reset()),e.data}catch(e){console.error(`Error: ${e}`)}}const g=new $(".gallery div ",{captionsData:"alt",captionDelay:250,alertError:!1}),q=document.querySelector(".form"),y=document.querySelector(".gallery"),a=document.querySelector(".loader"),l=document.querySelector(".load-btn");q.addEventListener("submit",H);l.style.display="none";a.style.display="none";let c,m,h;async function H(r){r.preventDefault(),a.style.display="block",y.innerHTML="";const t=r.currentTarget,n=t.elements.query.value.trim();if(n===""){p.show({title:"Error",color:"yellow",message:"Please search for something"}),l.style.display="none",a.style.display="none";return}f(n,15,1).then(o=>{h=o.totalHits,y.innerHTML=u(o),m=n,c=1,l.style.display="none",a.style.display="none",g.refresh(),t.reset()}).catch(o=>{console.error("Error:",o)})}l.addEventListener("click",async r=>{a.style.display="block",c+=1;try{const t=await f(m,15,c);c*15<h?(y.innerHTML+=u(t),g.refresh(),a.style.display="none",l.style.display="none",M()):p.show({title:"Info",timeout:2e3,color:"blue",position:"bottomRight",message:"We're sorry, but you've reached the end of search results."})}catch(t){console.error("Error:",t),alert(t.message)}finally{l.style.display="none"}});window.onscroll=function(){window.innerHeight+window.scrollY>=document.body.offsetHeight?(l.style.display="block",a.style.display="block"):(l.style.display="none",a.style.display="none")};function M(){const r=y.firstElementChild.getBoundingClientRect().height;window.scrollBy({top:2*r,behavior:"smooth"})}
+import{S as F,a as L,i as c}from"./assets/vendor-483db976.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();function B(o){return o.hits.map(({webformatURL:t,tags:r,largeImageURL:i,likes:e,views:s,comments:n,downloads:b})=>` <li>
+        <a class="gallery-link" href="${i}">
+        <img class="gallery-image" src="${t}" 
+        alt="${r}"
+        data-source="${i}">
+        <div class="image-info">
+                            <ul class="image-info-list">
+                                <li class="image-item">
+                                    <h2 class="image-text">Likes</h2>
+                                    <p class="image-quantity">${e}</p>
+                                </li>
+                                <li class="image-item">
+                                    <h2 class="image-text">Views</h2>
+                                    <p class="image-quantity">${s}</p>
+                                </li>
+                                <li class="image-item">
+                                    <h2 class="image-text">Comments</h2>
+                                    <p class="image-quantity">${n}</p>
+                                </li>
+                                <li class="image-item">
+                                    <h2 class="image-text">Downloads</h2>
+                                    <p class="image-quantity">${b}</p>
+                                </li>
+                            </ul>
+                        </div>
+      </li>`).join("")}function q(){return new F(".gallery-link",{captionsData:"alt",captionDelay:250})}const p=15;async function P(o,t){const r=new URLSearchParams({key:"42773289-557706e6fefb377aba1f00ed4",q:o,per_page:p,page:t}),i="https://pixabay.com/api/",e=await L.get(`${i}?${r}`);return{hits:e.data.hits,totalHits:e.data.totalHits}}const g=document.querySelector(".form"),y=document.querySelector(".gallery"),a=document.querySelector(".loader"),m=document.querySelector(".load-more");let u,l=1,d="",f=1;g.addEventListener("submit",S);m.addEventListener("click",v);function S(o){o.preventDefault();const t=g.elements.search.value;f=l,t!==d&&(l=1,d=t),y.innerHTML="",a.style.display="block",m.style.display="none",h(t,l),g.reset()}function v(){a.style.display="block",l+=1,l===f+1?h(d,l):console.log("Page was change...")}async function h(o,t){try{const r=await P(o,t);if(o.trim())if(r.hits.length===0){c.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#FFFFFF",backgroundColor:"#B51B1B",position:"center"}),a.style.display="none";return}else{const i=B(r);y.insertAdjacentHTML("beforeend",i),a.style.display="none",u?u.refresh():u=q(),w(),f=t;const e=r.totalHits,s=Math.ceil(e/p);if(t>=s)return m.style.display="none",c.error({position:"topRight",message:"We're sorry, there are no more images to load"});m.style.display="block"}else{c.error({message:"Please search for something",messageColor:"#FFFFFF",backgroundColor:"#B51B1B",position:"topRight"}),a.style.display="none";return}}catch{a.style.display="none",c.error({message:"Fetch error. Please try again later.",messageColor:"#FFFFFF",backgroundColor:"#B51B1B",position:"center"})}}function w(){const o=y.firstElementChild.getBoundingClientRect().height;window.scrollBy({top:2*o,behavior:"smooth"})}
 //# sourceMappingURL=commonHelpers.js.map
